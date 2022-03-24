307631e257b83d140edf8d7913f500724869db26
maigintomato <archatchanat.p@kkumail.com>
2022-03-25 02:00:49 +0700

    sprint5

--- editmeet.php
+++ editmeet.php
@@ -1,13 +1,16 @@
 <?php
+	session_start();
 	include('conn.php');
 	
 	$roomid=$_GET['roomid'];
 	
+	$id=$_POST['id'];
+	$title=$_POST['title'];
+	$head=$_POST['head'];
 	$roomname=$_POST['roomname'];
-	$location=$_POST['location'];
 
 	
-	mysqli_query($conn,"update room set roomname='$roomname', location='$location' where roomid='$roomid'");
-	header('location:addroom.php');
+	mysqli_query($conn,"update events set roomname='$roomname', location='$location' where id='$id'");
+	header('location:addmeet.php');
 
 ?>
--- meetaction.php
+++ meetaction.php
@@ -1,5 +1,5 @@
 <!-- Delete -->
-    <div class="modal fade" id="del<?php echo $row['roomid']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="del<?php echo $row['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -8,16 +8,17 @@
                 </div>
                 <div class="modal-body">
 				<?php
-					$del=mysqli_query($conn,"select * from room where roomid='".$row['roomid']."'");
+					$del=mysqli_query($conn,"select * from events where id='".$row['id']."'");
 					$drow=mysqli_fetch_array($del);
 				?>
+                
 				<div class="container-fluid">
-					<h5><center>roomname: <strong><?php echo $drow['roomname']; ?></strong></center></h5> 
+					<h5><center>title: <strong><?php echo $drow['title']; ?></strong></center></h5> 
                 </div> 
 				</div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-                    <a href="deleteroom.php?roomid=<?php echo $row['roomid']; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+                    <a href="deleteroom.php?roomid=<?php echo $row['id']; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Delete</a>
                 </div>
 				
             </div>
@@ -26,7 +27,7 @@
 <!-- /.modal -->
 
 <!-- Edit -->
-    <div class="modal fade" id="edit<?php echo $row['roomid']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="edit<?php echo $row['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -35,28 +36,47 @@
                 </div>
                 <div class="modal-body">
 				<?php
-					$edit=mysqli_query($conn,"select * from room where roomid='".$row['roomid']."'");
+					$edit=mysqli_query($conn,"select * from events where id='".$row['id']."'");
 					$erow=mysqli_fetch_array($edit);
 				?>
 				<div class="container-fluid">
-				<form method="POST" action="editroom.php?roomid=<?php echo $erow['roomid']; ?>">
-					<div class="row">
+				<form method="POST" action="editmeet.php?id=<?php echo $erow['id']; ?>">
+                <div class="row">
 						<div class="col-lg-2">
-							<label style="position:relative; top:7px;">username:</label>
+							<label style="position:relative; top:7px;">วาระการประชุม:</label>
 						</div>
 						<div class="col-lg-10">
-							<input type="text" name="roomname" class="form-control" value="<?php echo $erow['roomname']; ?>">
+							<input type="text" name="title" class="form-control" value="<?php echo $erow['title']; ?>">
 						</div>
 					</div>
-					<div style="height:10px;"></div>
-					<div class="row">
+                    <div style="height:10px;"></div>
+                    <div class="row">
 						<div class="col-lg-2">
-							<label style="position:relative; top:7px;">password:</label>
+							<label style="position:relative; top:7px;">ประธานการประชุม:</label>
 						</div>
 						<div class="col-lg-10">
-							<input type="text" name="location" class="form-control" value="<?php echo $erow['location']; ?>">
+							<input type="text" name="head" class="form-control" value="<?php echo $erow['head']; ?>">
 						</div>
 					</div>
+                    <div style="height:10px;"></div>
+                    <div class="row">
+						<div class="col-lg-2">
+							<label style="position:relative; top:7px;">จำนวนผู้เข้าร่วม: </label>
+						</div>
+						<div class="col-lg-10">
+							<input type="text" name="numattend" class="form-control" value="<?php echo $erow['numattend']; ?>">
+						</div>
+					</div>
+                    <div style="height:10px;"></div>
+					<div class="row">
+						<div class="col-lg-2">
+							<label style="position:relative; top:7px;">ห้องประชุม:</label>
+						</div>
+						<div class="col-lg-10">
+							<input type="text" name="roomid" class="form-control" value="<?php echo $erow['roomid']; ?>">
+						</div>
+					</div>
+                    
 
                 </div> 
 				</div>
